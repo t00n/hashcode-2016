29a1141eb6a03b034134c12396b529538d16af1e
iTitou <moiandme@gmail.com>
2016-02-11 19:14:35 +0100

    [enh] cleaner

--- coucou.py
+++ coucou.py
@@ -3,31 +3,40 @@
 from sys import argv
 from collections import namedtuple
 
+Problem = namedtuple('Problem',
+    ['products', 'warehouses', 'orders', 'rows', 'cols',
+     'n_drones', 'deadline', 'max_payload'])
 Warehouse = namedtuple('Warehouse', ['row', 'col', 'products'])
 Order = namedtuple('Order', ['row', 'col', 'products'])
 
-with open(argv[1]) as f:
-    r = lambda: f.readline().strip()
 
-    rows, cols, drones, deadline, max_payload = map(int, r().split())
-    n_products = int(r())
-    Products = map(int, r().split())
+def parse(filename):
+    with open(filename) as f:
+        r = lambda: f.readline().strip()
 
-    assert n_products == len(Products)
+        rows, cols, drones, deadline, max_payload = map(int, r().split())
+        n_products = int(r())
+        Products = map(int, r().split())
 
-    n_warehouses = int(r())
-    warehouses = []
-    for w in range(n_warehouses):
-        row, col = map(int, r().split())
-        products = map(int, r().split())
-        warehouses.append(Warehouse(row, col, products))
+        assert n_products == len(Products)
 
-    n_orders = int(r())
-    orders = []
-    for i in range(n_orders):
-        row, col = map(int, r().split())
-        _ = int(r())
-        products = map(int, r().split())
-        orders.append(Order(row, col, products))
+        n_warehouses = int(r())
+        warehouses = []
+        for w in range(n_warehouses):
+            row, col = map(int, r().split())
+            products = map(int, r().split())
+            warehouses.append(Warehouse(row, col, products))
 
-    print orders
+        n_orders = int(r())
+        orders = []
+        for i in range(n_orders):
+            row, col = map(int, r().split())
+            _ = int(r())
+            products = map(int, r().split())
+            orders.append(Order(row, col, products))
+
+        return Problem(Products, warehouses, orders, rows, cols,
+                       drones, deadline, max_payload)
+
+if __name__ == "__main__":
+    print parse(argv[1])

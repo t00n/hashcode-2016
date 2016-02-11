5ec363b3fe80b26dfe35e28b4cb17d58e77f8f5a
t00n <kelkununtel@hotmail.com>
2016-02-11 19:43:59 +0100

    [add] drones

--- solver.py
+++ solver.py
@@ -1,8 +1,17 @@
 from collections import namedtuple
+from parser import parse, Warehouse, Problem, Order
+from sys import argv
 
 class Load: pass
 class Deliver: pass
 class Unload: pass
 class Wait: pass
 
+data = parse(argv[1])
+
 Action = namedtuple('Action', ['drone', 'type', 'item', 'dest'])
+Drones = [(data.warehouses[0].row, data.warehouses[0].col) for i in range(data.n_drones)]
+
+def score(actions):
+    pass
+    
